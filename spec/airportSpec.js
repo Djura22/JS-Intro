76be97c3f94e21942f71b41d79ca9b46dfbf9390
Djura22 <michaeliangroves@gmail.com>
2020-09-01 17:50:33 +0100

    update

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -20,4 +20,17 @@ describe("Airport", function () {
       }).toThrow(new Error("Airport is full"));
     });
   });
+
+  describe("take-off", function () {
+    it("allows plane to takeoff from the airport", function () {
+      airport.land("plane");
+      airport.land("plane2");
+      airport.take_off("plane");
+      expect(airport.planes).toEqual(["plane2"]);
+    });
+  });
+
+
+
+
 });
